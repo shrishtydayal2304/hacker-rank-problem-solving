7e5fe90ba9443faf0797838f068796e5bd95dc8f
Shrishty Dayal <500068672@stu.upes.ac.in>
2020-07-15 20:43:22 +0530

    Create linkedlist_nodeinsertion.java

---
+++ linkedlist_nodeinsertion.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+      
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the insertNodeAtHead function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode insertNodeAtHead(SinglyLinkedListNode llist, int data) {
+
+/* i have created a new node object "toAdd" and then insertion of data .
+/*the toAdd is added at the starting of the list ,now next will be need to link it with the list nodes and the list is returned.
+        SinglyLinkedListNode toAdd = new SinglyLinkedListNode(data);
+        toAdd.next=llist;
+        llist = toAdd;
+        return llist;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
