ab89904e512b5ebf51c96e856e03e20e156cd81c
Shrishty Dayal <500068672@stu.upes.ac.in>
2020-07-16 20:47:43 +0530

    Update reverse linkedlist.java

--- reverse linkedlist.java
+++ reverse linkedlist.java
@@ -78,3 +78,27 @@ public class Solution {
     }
 
     private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        int tests = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int testsItr = 0; testsItr < tests; testsItr++) {
+            SinglyLinkedList llist = new SinglyLinkedList();
+
+            int llistCount = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < llistCount; i++) {
+                int llistItem = scanner.nextInt();
+                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+                llist.insertNode(llistItem);
+            }
+
+            reversePrint(llist.head);
+        }
+
+        scanner.close();
+    }
+}
+
