3fd7875f243db88019bf083d687d07e8b7d181e2
Shrishty Dayal <500068672@stu.upes.ac.in>
2020-07-18 15:04:09 +0530

    Create insert node.java

---
+++ insert node.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the insertNodeAtPosition function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode insertNodeAtPosition(SinglyLinkedListNode head, int data, int position) {
+        if(head==null)
+        return head;
+//    a node is created for the data to be inserted
+        SinglyLinkedListNode datanode = new SinglyLinkedListNode (data);
+        SinglyLinkedListNode current =head;
+        int index=0;
+        // travere till the  position one less then the actual whic  is curent.next
+        while(index<position-1){
+            current = current.next;
+            index++;
+        } 
+        // now move next to reach the excat position   and add the elementat the position
+        datanode.next=current.next;
+        current.next=datanode;
+        return head;
+    }
+
+
+        
+        
+    
+
+    private static final Scanner scanner = new Scanner(System.in);
