9a902f355e3408226e2960afffbeda90ce4d49c5
Shrishty Dayal <500068672@stu.upes.ac.in>
2020-07-23 19:14:32 +0530

    Create Print in reverse.java

---
+++ Print in reverse.java
@@ -0,0 +1,84 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the deleteNode function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode deleteNode(SinglyLinkedListNode head, int position) {
+        // if the linked list is empty then return the head reference
+        if(head==null)
+        return head;
+        // check the 0th postion and if we have to  delete the position 0 , then return the reference to the next node as the head
+        if(position == 0){
+            return head.next;
+        }
+        SinglyLinkedListNode current= head;
+        for(int i= 1;i<position;i++)
+        {
+            current=current.next;
+        }
+        current.next=current.next.next;
+        return head;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
